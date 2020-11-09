fc51b6e82ee3928ef6c8632a0d101e22d5455999
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-09 06:17:57 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,9 +63,8 @@ def add_characters
    # Code your solution here:
    
    epic_tragedy = :additional_characters => [
+     {:name => "Prince Escalus"},
      {:name => 
-       
-     }
      
      
      ]
